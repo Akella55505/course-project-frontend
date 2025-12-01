d8440a6967a7fb080a52967cb2fa87023f9bd480
Akella <leosaz0617@gmail.com>
2025-12-01 19:06:37 +0200

    chore: code cleanup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Home } from "../pages/Home";
+import { Home } from "../features/home/pages/Home.tsx";
 
 export const Route = createFileRoute("/")({
 	component: Home,
